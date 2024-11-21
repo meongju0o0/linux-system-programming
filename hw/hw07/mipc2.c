e3d048b28047f95bd4f4f90a6147362bd7a8a94b
meongju0o0 <bl4angel20@gmail.com>
2024-11-21 23:37:31 +0900

    hw07 finished

--- hw/hw07/mipc2.c
+++ hw/hw07/mipc2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "shm.h"
 #include "synclib.h"
 
 #define NLOOPS 10
@@ -11,7 +14,7 @@
 /*
 [Program Name]: mipc
 [Description]:
-	- mmap을 통해 counter 변수를 공유
+	- shared memory를 통해 ptr 포인터가 가르키는 영역을 공유
 	- 부모 자식 간에 번갈아가며 2씩 추가
 	- 파이프를 사용하여 부모 자식간에 counter 변수 동기화
 [Input]: None
@@ -23,7 +26,10 @@
 	- exit(int exit_code)
 	- close(int fd)
 	- fork()
-	- mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
+	- shmget(key_t key, size_t size, int shmflg)
+	- shmat(int shmid, const void *shmaddr, int shmflg)
+	- shmdt(const void *shmaddr)
+	- shmctl(int shmid, int cmd, struct shmid_ds *buf)
 	- TELL_WAIT()
 	- TELL_CHILD()
 	- WAIT_CHILD()
@@ -46,29 +52,20 @@ int update(int *ptr) {
 }
 
 int main() {
-	int fd, counter;
-	// file descripter 저장 변수
-	// counter 변수 선언(콘솔 출력용, 실제 업데이트는 area를 통해 이루어짐)
+	int counter; // counter 변수 선언(콘솔 출력용, 실제 업데이트는 ptr을 통해 이루어짐)
 	pid_t pid; // process id를 저장할 변수 선언
-	caddr_t area; // mmap을 통해 매핑한 메모리의 주소를 저장
 
-	if((fd = open("/dev/zero", O_RDWR)) < 0) { // mmap을 위해 /dev/zero 파일을 열어서 사용
-		perror("open"); // 실패한 경우 에러 메시지 출력
+	int shmid; // 공유 메모리 id를 저장할 변수 선언
+	int *ptr; // 공유 메모리 시작 주소를 저장할 변수
+
+	if((shmid = shmget(SHM_KEY, SIZE, SHM_MODE)) < 0) { // 공유 메모리 할당, 공유 메모리 id 저장
+		perror("shmget"); // 공유 메모리 할당에 실패한 경우 에러 메시지 출력
 		exit(1); // 비정상 종료
 	}
-	if((area = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == (caddr_t)-1) { // area와 fd 메모리 매핑
-	/*
-	0: 커널이 매핑할 메모리 주소를 선택
-	SIZE: 파일 크기
-	PROT_READ: 읽기 모드
-	MAP_SHARED: 파일과 메모리 동기화
-	fd: file descripter
-	0: offset
-	*/
-		perror("mmap"); // 실패한 경우 에러 메시지 출력
+	if((ptr = shmat(shmid, 0, 0)) == (void *)-1) { // 할당된 공유 메모리에 접근할 가상 메모리 주소 할당
+		perror("shmat"); // 가상 메모리 주소 할당에 실패한 경우 에러 메시지 출력
 		exit(1); // 비정상 종료
 	}
-	close(fd); // file descripter 종료
 
 	TELL_WAIT(); // 파이프 생성
 	if((pid = fork()) < 0) { // 자식 프로세스 생성
@@ -77,7 +74,7 @@ int main() {
 	}
 	else if(pid > 0) { // 부모 프로세스이면
 		for(int i = 0; i < NLOOPS; i += 2) { // i를 2씩 증가 시키며(전체 반복 횟수의 절반)
-			if((counter = update((int *)area)) != i) { // counter 변수 update
+			if((counter = update(ptr)) != i) { // counter 변수 update
 				fprintf(stderr, "Counter mismatch\n"); // update에 실패한 경우 에러 메시지 출력
 				// 예상되는 증가량인 2와 불일치하는 경우가 에러
 				exit(1); // 비정상 종료
@@ -90,13 +87,26 @@ int main() {
 	else { // 자식 프로세스이면
 		for(int i = 1; i < NLOOPS; i += 2) { // i를 2씩 증가 시키며(전체 반복 횟수의 절반)
 			WAIT_PARENT(); // 부모 프로세스로부터의 메시지 대기
-			if((counter = update((int *)area)) != i) { // counter 변수 update
+			if((counter = update(ptr)) != i) { // counter 변수 update
 				fprintf(stderr, "Counter mismatch\n"); // update에 실패한 경우 에러 메시지 출력
 				// 예상되는 증가량인 2와 불일치하는 경우가 에러
 				exit(1); // 비정상 종료
 			}
 			printf("Child: counter=%d\n", counter); // 카운터 변수 출력
 			TELL_PARENT(); // 부모 프로세스에 메시지 전송
+		}
+	}
+
+	if(shmdt(ptr) < 0) { // shmptr과 공유 메모리 연결 해제(부모, 자식 프로세스 모두 실행)
+		perror("shmdt"); // 해제에 실패한 경우 에러 메시지 출력
+		exit(1); // 비정상 종료
+	}
+
+	struct shmid_ds shm_info;
+	if(shmctl(shmid, IPC_STAT, &shm_info) == 0 && shm_info.shm_nattch == 1) { // 공유 메모리에 attach된 프로세스가 없는 경우
+		if(shmctl(shmid, IPC_RMID, NULL) < 0) { // 공유 메모리 할당 해제
+			perror("shmctl"); // 해제에 실패한 경우 에러 메시지 출력
+			exit(1); // 비정상 종료
 		}
 	}
 
--- hw/hw07/mycp3.c
+++ hw/hw07/mycp3.c
@@ -7,48 +7,102 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/*
+[Program Name]: mycp3
+[Description]: mmap을 활용하여 파일 복사 기능을 구현
+	- argv[1] 파일을 argv[2]에 복사
+		- ./mycp3 argv[1] argv[2]
+	- argv[1] 파일을 RDONLY 모드로 엶(fdin)
+	- argv[2] 파일을 RDWR, CREAT, TRUNC 모드로 엶, 접근 권한: 0644(fdout)
+	- argv[2] 파일의 크기를 argv[1] 파일과 동일하게 설정
+		- by using
+			- lseek(int fd, off_t offset, int shence)
+			- write(int fd, const void *buf, size_t count)
+	- src를 argv[1]과 매핑하여 메모리와 파일 간 동기화
+	- dst를 argv[2]와 매핑하여 메모리와 파일 간 동기화
+	- dst에 src를 복사하여 argv[2]로 argv[1] 파일을 복사
+[Input]: src 파일, dst 파일
+[Output]: src를 복사한 dst 파일
+[Calls]:
+	- fprintf(FILE *stream, const char *format, ...)
+	- perror(char *err_msg)
+	- exit(int exit_code)
+	- open(const char *pathname, int flags)
+	- open(const char *pathname, int flags, mode_t mode)
+	- fstat(int fd, struct stat *statbuf)
+	- lseek(int fd, off_t offset, int whence)
+	- write(int fd, const void *buf, size_t count)
+	- mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
+	- memcpy(void *dest, const void *src, size_t n)
+	- close(int fd)
+*/
+
 int main(int argc, char *argv[]) {
-	int fdin, fdout;
-	char *src, *dst;
-	struct stat statbuf;
+	int fdin, fdout; // file descripter 번호를 저장할 변수 선언
+	char *src, *dst; // mmap을 통해 매핑할 변수 선언
+	// src와 argv[1] 매핑, dst와 argv[2] 매핑
+	struct stat statbuf; // file의 stat을 저장할 구조체 변수 선언
 
-	if(argc != 3) {
-		fprintf(stderr, "Usage: %s source destination\n", argv[0]);
-		exit(1);
-	}
-
-	if((fdin = open(argv[1], O_RDONLY)) < 0) {
-		perror("open");
-		exit(1);
-	}
-	if((fdout = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0) {
-		perror("open");
-		exit(1);
+	if(argc != 3) { // argument의 개수가 잘못된 경우 예외처리
+		fprintf(stderr, "Usage: %s source destination\n", argv[0]); // 에러 메시지 출력
+		exit(1); // 비정상 종료
 	}
 
-	if(fstat(fdin, &statbuf) < 0) {
-		perror("fstat");
-		exit(1);
+	if((fdin = open(argv[1], O_RDONLY)) < 0) { // argv[1]을 읽기 전용 모드로 엶
+		perror("open"); // 파일 열기에 실패한 경우 에러 메시지 출력
+		exit(1); // 비정상 종료
 	}
-	if(lseek(fdout, statbuf.st_size - 1, SEEK_SET) < 0) {
-		perror("lseek");
-		exit(1);
+	if((fdout = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0) { // argv[1]을 주어진 모드에 따라 엶
+	/*
+	O_RDWR: 읽기, 쓰기
+	O_CREAT: 파일이 없는 경우 새로 생성
+	O_TRUNC: 파일이 있는 경우 기존 내용을 모두 삭제
+	0644: -rw-r--r--
+	*/
+		perror("open"); // 파일 열기에 실패한 경우 에러 메시지 출력
+		exit(1); // 비정상 종료
 	}
-	write(fdout, "", 1);
 
-	if((src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0)) == (caddr_t)-1) {
+	if(fstat(fdin, &statbuf) < 0) { // argv[1] 파일의 정보를 가져옴
+		perror("fstat"); // 실패한 경우 에러 메시지 출력
+		exit(1); // 비정상 종료
+	}
+	if(lseek(fdout, statbuf.st_size - 1, SEEK_SET) < 0) { // argv[2] 파일의 오프셋을 argv[1] 파일 크기 -1로 이동
+		perror("lseek"); // 실패한 경우 에러 메시지 출력
+		exit(1); // 비정상 종료
+	}
+	write(fdout, "", 1); // 바로 다음 오프셋에 빈 문자열 입력
+
+	if((src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0)) == (caddr_t)-1) { // src와 fdin 메모리 매핑
+	/*
+	0: 커널이 매핑할 메모리 주소를 선택
+	statbuf.st_size: 파일 크기
+	PROT_READ: 읽기 모드
+	MAP_SHARED: 파일과 메모리 동기화
+	fdin: file descripter
+	0: offset
+	*/
 		perror("mmap");
 		exit(1);
 	}
-	if((dst = mmap(0, statbuf.st_size, PROT_WRITE, MAP_SHARED, fdout, 0)) == (caddr_t)-1) {
+	if((dst = mmap(0, statbuf.st_size, PROT_WRITE, MAP_SHARED, fdout, 0)) == (caddr_t)-1) { // dst와 fdout 메모리 매핑
+	/*
+	0: 커널이 매핑할 메모리 주소를 선택
+	statbuf.st_size: 파일 크기
+	PROT_WRITE: 쓰기 모드
+	MAP_SHARED: 파일과 메모리 동기화
+	fdout: file descripter
+	0: offset
+	*/
 		perror("mmap");
 		exit(1);
 	}
 
-	memcpy(dst, src, statbuf.st_size);
+	memcpy(dst, src, statbuf.st_size); // dst에 src의 내용 복사
+	// 메모리 복사와 함께 매핑된 파일에 dst의 내용이 argv[1]에 쓰임
 
-	close(fdin);
-	close(fdout);
+	close(fdin); // fdin file descripter 종료
+	close(fdout); // fdout file descripter 종료
 
 	return 0;
 }
